5c237a1cc357bc5e76ceeb215bde89f07618a9e6
homsau <james.p.cowart@gmail.com>
2018-06-20 21:19:46 -0400

    commit

--- assets/javascript/gifs.js
+++ assets/javascript/gifs.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
     function addButtons() {
         $("#spButtons").empty();
         for (i = 0; i < characters.length; i++) {
-            var buttons = $("<button>").addClass("characterBtn").attr("data-name", characters[i]).text(characters[i]);
+            var buttons = $("<button>").addClass("characterBtn").attr("data-name", characters[i]).text(characters[i]).css("textTransform", "capitalize");
             $("#spButtons").append(buttons);
         };
     };
@@ -22,12 +22,11 @@ $(document).ready(function() {
         $("#spCharacterSubmit").prop("disabled", this.value == "" ? true : false);
     });
 
-    // add click function
+    // add click function for adding a new button
     $("#spCharacterSubmit").on("click", function(event) {
         event.preventDefault();
         var spCharacter = $("#spInput").val().trim();
-        $(spCharacter).css("textTransform", "capitalize");
-	    characters.push(spCharacter).css("textTransform", "capitalize");
+	    characters.push(spCharacter);
         $("#spInput").val("");
         $("#spCharacterSubmit").prop("disabled",true);
         $("#spInput").keyup(function(){
@@ -38,11 +37,15 @@ $(document).ready(function() {
         getData();
     });
 
+    // add click function after page load to grab info
     function getData() {
         $("button").on("click", function() {
             // grabbing and storing the data-name property value from the button
             var name = $(this).attr("data-name");
-            
+            // the line below was going to clear the gifs so that the page wouldn't get
+            // really long on each button click... according to the instructions
+            // it should just add them
+            //$("#spGIFs").html("");
             // api/ajax function to get response
             console.log(name);
             var gifURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=sblaAmKxYnbI6e15gS95XuAMeqpbV64E";
@@ -54,30 +57,48 @@ $(document).ready(function() {
                 var results = response.data;
                 console.log(results);
                 // looping through each result item
-                for (var i = 0; i < results.length; i++) {
-
+                for (var i = 0; i < 10; i++) {
                 // creating and storing a div tag
-                var characterDiv = $("<div>");
-
-                // Creating a paragraph tag with the result item's rating
-                var p = $("<p>").text("Rating: " + results[i].rating);
-
-                // Creating and storing an image tag
-                var characterImage = $("<img>");
-                
-                // Setting the src attribute of the image to a property pulled off the result item
-                characterImage.attr("src", results[i].images.fixed_height.url);
-
-                // Appending the paragraph and image tag to the animalDiv
-                characterDiv.append(p);
+                var characterDiv = $("<div>").addClass("gifDiv");
+                // create a p tag for displaying the rating
+                var rating = (results[i].rating).toUpperCase();
+                var p = $("<p>").html("Rating: " + rating);
+                // creating and storing an image tag which is the gif
+                var characterImage = $("<img>").attr({
+                    class : "gif",
+                    //"data-state" : "still",
+                    "src" : results[i].images.fixed_height.url,
+                    "data-still" : results[i].images.fixed_height_still.url,
+                    "data-animate" : results[i].images.fixed_height.url
+                });
+                //$(".gif").attr("data-state", "still");
+                // setting the src attribute of the image to a property pulled off the result item
+                // characterImage.attr("src", results[i].images.fixed_height.url);
+                // characterImage.attr(data-state, "still");
+                // appending the paragraph and image tag to the spGIFs div
                 characterDiv.append(characterImage);
-
-                // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+                characterDiv.append(p);
+                // prepend the gifs to the HTML page in the spGIFs div
                 $("#spGIFs").prepend(characterDiv);
                 }
-            });
+            }); 
         });
-    // add click function after page load
-    // animate// Creates variables
+        //$(document).on("click", ".gif", animateGIFs);
     } getData();
+
+    // animate
+    $(document).on("click", ".gif", function () {
+        //$(".gif").on("click", function() {
+        var state = $(this).attr("data-state");
+        // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+        // Then, set the image's data-state to animate
+        // Else set src to the data-still value
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
 });
